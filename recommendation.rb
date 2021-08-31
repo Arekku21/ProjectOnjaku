5922b9947ec63040ed7369bc76cf65e5b215cd85
Alec Vince <70086993+Arekku21@users.noreply.github.com>
2021-08-31 20:59:56 +0800

    Create recommendation.rb

---
+++ recommendation.rb
@@ -0,0 +1,63 @@
+#genre random recommendation
+def rfunc1()
+    marray=["Faith Fiance - Tell NoBody","SZA - Love Galore (ft.Travis Scott)","Jhene Aiko - Triggered","Summer Walker - Come Thru","Frank Ocean - White Ferari","Kehlani - Everybody Business","Mariah Carey - Fantasy","Beyonce - Dangerously in love","Victoria Monet - Dive"]
+
+    var="R&B-" + marray[rand(0..9)].to_s
+    return var
+end    
+def rfunc2()
+    marray=["Ariana Grande - into you","Taylor Swift - You need to calm down","Lady Gaga - Rain on Me","Britney Spears - Give me more","Katy Perry - California girl","Zara Larson - All the time","Bazzi - IFLY","Rihanna - Work","Lauv - Never Not"]
+    var="Pop-"+marray[rand(0..9)].to_s
+    return var
+end
+def rfunc3()
+    marray=["Drake - Passion Fruit","Nicki Manaj - All things go","6lack - Float","Big Sean - Bounce Back","Brock Hamton - Sugar","Qveen Herby - BDE","Chance the rapper - Hot Shower","Mac Miller - 2009","Russ - Psycho pt.2","Kota the friend - Colarado"]
+    var="Rap-"+marray[rand(0..9)].to_s
+    return var
+end
+def rfunc4()
+    marray=["Rich Brian - 100 Degrees","A boogie Wit da Hoodie - Swervin(ft.6ix9ine)","Doja Cat - Cybersex","Lil Wayne - How to love","Tyga - Ayy Macarena)","Travis Scott - Sicko Mode","G-Eazy - Me,Myself & i(w/Bebe RExha)","Wiz Khalifa - See you again(ft.Charlie Puth)","Juice WRLD - Lucid Dreams","21 Savage - a lot"]
+    var="HipHop-"+marray[rand(0..9)].to_s
+    return var
+end
+def rfunc5()
+    marray=["B.B King - The Thrill is Gone","Muddy Waters - Mannish Boy","Robert Johnson - Cross Road Blues","John Lee Hooker - Boom Boom","Stevie Ray Vaughan - Pride and Joy","Howlin' Wolf - Smokestack Lightnin'","Buddy Guy - Feels Like Rain","Albert King - Born Under a Bad Sign-Mono mix","Lightnin' Hopkins - Mojo Hands","Elmore James - The Sky is Crying"]
+    var="Blues-"+marray[rand(0..9)].to_s
+    return var
+end
+def rfunc6()
+    marray=["Green Day - Wake me Up in September","My Chemical Romance - Helena","Evenescence - Bring Me To Life","Bon Jovi - Always","Prince - Purple Rain","Michael Learns to Rock - You Took My Heart Away","Aerosmith - I Don't Want to Miss a Thing","Paramore - Still into you","Coldplay - Fix You","Panic! at the disco - Into the Unknown",]
+    var="Rock & Roll-"+marray[rand(0..9)].to_s
+    return var
+end
+def rfunc7()
+    marray=["Zedd - Happy Now","David Guetta - Titanium(ft.Sia)","Alan Walker - Faded","Avicii - Wake Me Up","Marshmello - Silence(ft.Khalid)","Kygo - Stargazing(w/Justin Jesso)","Jonas Blue - Fast Car(w/Dakato)","Calvin Harris - This is what you came for","Cheat Codes - No Promises(w/Demi Lovato)","Martin Garrix - Scared to Be Lonely(w/Dua Lipa)"]
+    var="EDM-"+marray[rand(0..9)].to_s
+    return var
+end
+def rfunc8()
+    marray=["Billy Eilish - everything i wanted","Lana del Ray - Happiness is a Butterfly","Troy Sivan - Strawherries & Cigarettes","Florence+the machine - Dog Days Are Over","Sia - Eye of the needle","The Killers - Mr.Brightside","Bjork - It's Oh so Quiet","Lorde - Royals","Melanie Martinez - Play Date","Halsey - Without Me"]
+    var="Alternative-"+marray[rand(0..9)].to_s
+    return var
+end
+def rfunc9()
+    marray=["Jeremy Zucher - always I'll care","Harry Styles - Adore you","FINNEAS - Let's Fall in Love for the Night","Yuna - Crush","the surfaces - Sunday Best","Lewis Capaldi - Before yoo go","Honne - Location Unknown(w/Georgia)","Conan Gray - Maniac","1975 - Somebody Else","LANY - Mean It(w/Lauv)",]
+    var="Indie-"+marray[rand(0..9)].to_s
+    return var
+end
+def rfunc10()
+    marray=["Taylor Swift - Mean","Dolly Parton - 9 to 5","Kenny Rogers - Island in the Stream","Cassadee Pope - Wasting All These Years","LeAnn Rimes - How Do I live","Kacey Musgraves - Rainbow","Rascal Flatts - Life is A Highway","Carry Underwood - Do you hear what I hear","Brad Paisley - She's Everything","Tim Mcgraw - Humble And Kind"]
+    var="Country"+marray[rand(0..9)].to_s
+    return var
+end
+def rfunc11()
+    marray=["Whitney Houston - I am every woman","Aretha Franklin - Think","Nataalie Cole - L-O-V-E","the Jackson 5 - I'll be there","The Supremes - Someday We'll be together","Al Green - Listen to me ","Stevie Wonder - I Just Called To Say I Love You","The Temptations - Papa was A Rollin' Stone","Diana Ross - Endless Love","Marvin Gaye - Ain't No Mountain High Enough"]
+    var="Soul-"+marray[rand(0..9)].to_s
+    return var
+end   
+def rfunc12()
+    marray=["LiSA - crossing field", "春奈るな　- Overfly", "ASCA - Resister", "ReoNa - forget-me-not", "THE ORAL CIGARETTES - 狂乱Hey Kids!!", "Hello Sleepwalkers - 午夜の待ち合わせ", "ヨルシカ　- ただ君に晴れ","ヨルシカ　- だから僕は音楽を辞めた","LiSA - Oathsign"]
+    var="Japanese Songs-"+marray[rand(0..9)].to_s
+    return var
+end
+
